1edad4d747ad50aa739511b4b091ddb4a16ce02d
Muhammadhussain1999 <ksaad7933@gmail.com>
2023-11-12 14:42:45 +0500

    EndGamePart1

--- sheriasBE/Basic_Setup_Express_Mongoose/script.js
+++ sheriasBE/Basic_Setup_Express_Mongoose/script.js
@@ -3,6 +3,7 @@
 
 const express = require("express");
 const app = express();
+const userModel = require("./users");
 app.use(function (req, req, next) {
   console.log("hello from middleware");
   next();
---
+++ sheriasBE/Basic_Setup_Express_Mongoose/users.js
@@ -0,0 +1,15 @@
+const mongoose = require("mangoose");
+
+// dataBase Creation
+mongoose.connect("mangodb://127.0.0.1:27017/MyFirstDataBase");
+
+// Schema matlab appko ye btana banne waala har document mein kya kya hoga
+
+const userSchema = mangoose.Schema({
+  username: String,
+  name: String,
+  age: Number,
+});
+
+//collection
+model.exports = mangoose.model("user", userSchema);
