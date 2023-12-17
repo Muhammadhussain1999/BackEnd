26a58983cdc022afab2570355a1b0a27548a48d5
Muhammadhussain1999 <ksaad7933@gmail.com>
2023-12-17 21:56:38 +0500

    adding profile and feed pages

--- sheriasBE/project_pintrest_app/Pinterest/routes/index.js
+++ sheriasBE/project_pintrest_app/Pinterest/routes/index.js
@@ -1,9 +1,52 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const userModel = require("./users");
+const postModel = require("./posts");
+const passport=require('passport')
+const localStrategy = require("passport-local");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+passport.use(new localStrategy(userModel.authenticate()));
+router.get("/", function (req, res, next) {
+  res.render("index");
+});
+router.get("/login", function (req, res, next) {
+  res.render("login");
 });
 
+router.get('/profile', isLoggedIn,function(req,res){
+  res.render("profile")
+})
+router.get("/feed",function(req,res){
+  res.render("feed")
+})
+router.post("/register", function (req, res) {
+  const { username, email, fullname } = req.body;
+  const userData = new userModel({ username, email, fullname });
+  userModel.register(userData, req.body.password).then(function () {
+    passport.authenticate("local")(req, res, function () {
+      res.redirect("/profile");
+    });
+  });
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/profile",
+    failureRedirect: "/",
+  }),
+  function (req, res) {}
+);
+
+router.get('/logout', function(req, res, next){
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+});
+
+function isLoggedIn(req,res,next){
+  if(req.isAuthenticated())return next()
+  res.redirect("/")
+}
 module.exports = router;
--- sheriasBE/project_pintrest_app/Pinterest/routes/users.js
+++ sheriasBE/project_pintrest_app/Pinterest/routes/users.js
@@ -1,9 +1,31 @@
-var express = require('express');
-var router = express.Router();
+const mongoose = require('mongoose');
+const plm=require("passport-local-mongoose")
+mongoose.connect("mongodb://127.0.0.1:27017/MySetupForPintreset")
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+  },
+  posts: [{type: mongoose.Schema.Types.ObjectId,
+  ref: 'Post'}],
+  dp: {
+    type: String // Assuming you store the URL or path to the user's profile picture
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  fullname: {
+    type: String
+  }
 });
+userSchema.plugin(plm)
+module.exports = mongoose.model('User', userSchema);
 
-module.exports = router;
+
